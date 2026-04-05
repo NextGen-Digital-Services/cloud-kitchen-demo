95027cc0059c1c13bd74156b21a7164031a4dbee
NextGen Digital Services <nextgendigitalservices2026@gmail.com>
2026-04-05 23:50:46 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Menu from "../components/Menu";
+import Popular from "../components/Popular";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Menu />
+      <Popular />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
